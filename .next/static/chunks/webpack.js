32c8117ae41aa62af79b76c914531c2c50a38693
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-02-26 16:43:29 +0100

    Move sign-up to a separate page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3ed355f2e18a740"; }
+/******/ 		__webpack_require__.h = function() { return "dc798a04dfa2ec82"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
