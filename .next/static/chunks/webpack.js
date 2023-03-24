4b6b789671fa1d59444e3720763e964f55390712
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-03-24 22:08:51 +0100

    Ignore case off in git config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "869a7791878a21ec"; }
+/******/ 		__webpack_require__.h = function() { return "50493f3764c0786f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
