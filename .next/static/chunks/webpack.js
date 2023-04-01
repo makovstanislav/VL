772733d67629739502fba92cb1fa0b4e4015ab42
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-04-01 10:44:19 +0200

    Add UI for searching samples

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee397dd3d889d100"; }
+/******/ 		__webpack_require__.h = function() { return "68f3bfbb2c0381f9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.5f0de50e3159d0b8.hot-update.js
+++ .next/static/webpack/webpack.5f0de50e3159d0b8.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "4f7140997b0a1d98"; }
+/******/ 	__webpack_require__.h = function() { return "82fcf50db845de97"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
